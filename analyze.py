d6039e71c75b605d150d64ac7dda006ce4fd12a7
jeff <jeffreyzhuu@gmail.com>
2024-11-21 19:51:13 -0800

    inflation rate comparing to percentage the basket of food is to there monthly salary (based off minimum wage and 160hr/month)

--- analyze.py
+++ analyze.py
@@ -5,6 +5,7 @@ from scipy import stats
 import sys
 
 filename1 = sys.argv[1]
+filename2 = sys.argv[2]
 
 def contains(string, target):
     if target not in string:
@@ -22,6 +23,8 @@ fit = stats.linregress(data['minimum_wage'].values, data['inflation_change'].val
 print(f"Correlation coefficient: {fit.rvalue}")
 
 data['residual'] = data['inflation_change'] - (data['minimum_wage'] * fit.slope + fit.intercept)
+data = data.reset_index()
+print(data)
 
 plt.figure(1)
 plt.plot(data['minimum_wage'].values, data['inflation_change'].values, 'b.', alpha=0.5)
@@ -32,4 +35,25 @@ plt.ylabel('Inflation Rate')
 
 data.plot.hist(column=['residual'])
 print(f"Residual normality test: {stats.normaltest(data['residual'].values).pvalue}")
-plt.show()
+# plt.show()
+
+data2 = pd.read_csv(filename2, parse_dates=['date'])
+basket = pd.DataFrame([])
+basket['date'] = data2['date']
+basket['price'] = data2['price'] * 8 # basket for 1 month
+basket = basket.groupby(by=['date']).agg(sum=('price', 'sum'))
+basket = basket.iloc[:-(len(basket) - len(data))]
+
+
+
+joint = pd.merge(data, basket, how='right', on='date')
+joint = joint.dropna()
+joint['percentage_salary'] = (joint['sum'])/(joint['minimum_wage'] * 160) * 100
+
+high_percentage_of_salary_neg_inflation = joint[(joint['percentage_salary'] > 18.5) & (joint['inflation_change'] < 0)]
+low_percentage_of_salary_neg_inflation = joint[(joint['percentage_salary'] <= 18.5) & (joint['inflation_change'] < 0)]
+high_percentage_of_salary_pos_inflation = joint[(joint['percentage_salary'] > 18.5) & (joint['inflation_change'] >= 0)]
+low_percentage_of_salary_pos_inflation = joint[(joint['percentage_salary'] <= 18.5) & (joint['inflation_change'] >= 0)]
+
+print(stats.chi2_contingency([[len(high_percentage_of_salary_pos_inflation), len(low_percentage_of_salary_pos_inflation)], [len(low_percentage_of_salary_pos_inflation), len(low_percentage_of_salary_neg_inflation)]]).pvalue)
+
